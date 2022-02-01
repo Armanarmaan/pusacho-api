2002902df9be328101d0a5795a18f367414b939d
Arman <arman.41@hotmail.com>
2022-02-01 14:03:45 +0700

    fixing verifytoken

--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -8,23 +8,63 @@ module.exports = function(req,res,next){
   
   try{
     const req_role_split = req_role.split(',');
-    let verified;
-    req_role_split.forEach(item => {
+    if( req_role_split[0] === '0' ){
       try {
-        const token_secret = item === '0' ? process.env.TOKEN_SECRET_MANAJEMEN : item === '1' ? process.env.TOKEN_SECRET_LAPANGAN : process.env.TOKEN_SECRET_HYBRID;
-        verified = jwt.verify(token, token_secret);
-      } catch (error) { }
-    });
-    if(verified){
-      req.user = verified;
-      next();
+        const token_secret = process.env.TOKEN_SECRET_MANAJEMEN;
+        let verified = jwt.verify(token, token_secret);
+        req.user = verified;
+        next();
+      } catch (error) {
+        if( req_role_split[1] === '2' ){
+          try {
+            const token_secret = process.env.TOKEN_SECRET_HYBRID;
+            let verified = jwt.verify(token, token_secret);
+            req.user = verified;
+            next();
+          } catch (error) {
+            res.status(400).json({
+              status: 400,
+              data: 'token invalid'
+            })
+          }
+        }
+        else{
+          res.status(400).json({
+            status: 400,
+            data: 'token invalid'
+          })
+        }
+      }
     }
-    else{
-      res.status(400).json({
-        status: 400,
-        data: 'token invalid'
-      })
+    else if( req_role_split[0] === '1' ){
+      try {
+        const token_secret = process.env.TOKEN_SECRET_LAPANGAN;
+        let verified = jwt.verify(token, token_secret);
+        req.user = verified;
+        next();
+      } catch (error) {
+        if( req_role_split[1] === '2' ){
+          try {
+            const token_secret = process.env.TOKEN_SECRET_HYBRID;
+            let verified = jwt.verify(token, token_secret);
+            req.user = verified;
+            next();
+          } catch (error) {
+            res.status(400).json({
+              status: 400,
+              data: 'token invalid'
+            })
+          }
+        }
+        else{
+          res.status(400).json({
+            status: 400,
+            data: 'token invalid'
+          })
+        }
+      }
     }
+    
   } catch(err){
     console.log(err);
     res.status(400).json({
